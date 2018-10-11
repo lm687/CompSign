e7d7e38e6b3f610c75cc4e3fc0999c177cbc35b2
Lena M <lm687@cam.ac.uk>
2018-10-11 16:33:10 +0100

    add linear model; fix bugs

--- R/ROO_signatures.R
+++ R/ROO_signatures.R
@@ -75,7 +75,7 @@ comp_lm <- function(x, indices_predictor){
 
   #require(compositions)
   apply(compositions::ilr(compositions::acomp(x@count_matrix)), 2, function(it_ilr){
-   model_compReg <- compositions::lm(unlist(it_ilr~(x@df)[,indices_predictor]))
+   model_compReg <- lm(unlist(it_ilr~(x@df)[,indices_predictor]))
    list(summary(model_compReg), anova(model_compReg))
   })
 }
@@ -85,7 +85,7 @@ comp_lm <- function(x, indices_predictor){
   if(class(x)[1] != 'merged_compositional') stop('Input must be of class <merged_compositional>')
 
   #require(compositions)
-  model_compReg <- compositions::lm(compositions::ilr(compositions::acomp(x@count_matrix))~(x@df)[,indices_predictor])
+  model_compReg <- lm(compositions::ilr(compositions::acomp(x@count_matrix))~as.matrix((x@df)[,indices_predictor]))
   list(summary(model_compReg)#,
        #anova(model_compReg)
        )
@@ -189,12 +189,6 @@ setGeneric("within_signature_analysis", function(obj, ...) standardGeneric("with
 # dum_sign@id_samples
 # dum_metadata@df
 #
-# tmp_merged_compositional <- new("merged_compositional",
-#                                 id='adas',
-#                                 id_samples=c("sam1", "sam2", "sam3"),
-#                                 id_signatures= c('s1', 's2', 's3', 's4'), ## signature names
-#                                 count_matrix=MCMCpack::rdirichlet(3, c(1,1,1,1)),
-#                                 df=data.frame(a=c(3,4,1), b=c(10, 10, 10)))
 
 link_to_clinical_data <- function(predictors, response){
   ## the predictors are the signatures (compositional)
@@ -236,52 +230,57 @@ link_to_clinical_data <- function(predictors, response){
 
 }
 
-# link_to_clinical_data(dum_sign, dum_metadata)
-#
-# comp_lm(tmp_merged_compositional, 1)
-#
-#
-# dum_sign <- new("sign",
-#                 id="dum1",
-#                 id_samples=c("sam1", "sam2", "sam3"),
-#                 id_signatures= c('s1', 's2', 's3', 's4'), ## signature names
-#                 count_matrix=MCMCpack::rdirichlet(3, c(1,1,1,1)),
-#                 modified=FALSE)
-# d <- readRDS("~/Desktop/tcga_ov_signature_exposures_20180926.rds")
-# many_tcga <- new("sign",
-#                  id='many_tcga',
-#                  id_samples=rownames(d),
-#                  id_signatures= colnames(d), ## signature names
-#                  count_matrix=d,
-#                  modified=FALSE)
-#
-# ## TODO: add types of response (categorical, numerical, ...)
-# dum_metadata <- new("metadata",
-#                     id="dum1",
-#                     id_samples=c("sam1", "sam2", "sam3"),
-#                     df=data.frame(age=sample(1L:100L,3),
-#                                   some_cat=as.character(letters[sample(1L:length(letters), 3)]),
-#                                   stringsAsFactors = FALSE)
-# )
+#########################################
+############### DEBUGGING ###############
+#########################################
 
-### Example of matrix transformed into sign object
-# aaa <- matrix(runif(100), 4)
-# colnames(aaa) <- paste0('s', 1:25); rownames(aaa) <- paste0('sam', 1:4)
-# to_sign(aaa)
-#
-# input_dummy <- matrix(runif(100), 4)
-# colnames(input_dummy) <- paste0('s', 1:25); rownames(input_dummy) <- paste0('sam', 1:4)
-# sign_dummy <- to_sign(input_dummy)
-# add_together_matrix(sign_dummy)
-# summarise(add_together_matrix(sign_dummy))
-#
-# obj <- add_together_matrix(sign_dummy)
-# list(General=paste0("Object of class ", class(obj)),
-#      `Number of signatures` = ncol(obj@count_matrix),
-#      `Number of samples` = nrow(obj@count_matrix),
-#      `Geometric means of signatures`= sort(apply(obj@count_matrix, 2, function(x) exp(mean(log(x)))), decreasing=TRUE),
-#      `Covariance` = compositions::var.acomp(obj@count_matrix))
-# summarise(obj)
+link_to_clinical_data(dum_sign, dum_metadata)
+
+comp_lm(tmp_merged_compositional, 1)
 
 
-# summarise(to_sign(aaa))
+dum_sign <- new("sign",
+                id="dum1",
+                id_samples=c("sam1", "sam2", "sam3"),
+                id_signatures= c('s1', 's2', 's3', 's4'), ## signature names
+                count_matrix=MCMCpack::rdirichlet(3, c(1,1,1,1)),
+                modified=FALSE)
+d <- readRDS("~/Desktop/tcga_ov_signature_exposures_20180926.rds")
+many_tcga <- new("sign",
+                 id='many_tcga',
+                 id_samples=rownames(d),
+                 id_signatures= colnames(d), ## signature names
+                 count_matrix=d,
+                 modified=FALSE)
+
+## TODO: add types of response (categorical, numerical, ...)
+dum_metadata <- new("metadata",
+                    id="dum1",
+                    id_samples=c("sam1", "sam2", "sam3"),
+                    df=data.frame(age=sample(1L:100L,3),
+                                  some_cat=as.character(letters[sample(1L:length(letters), 3)]),
+                                  stringsAsFactors = FALSE)
+)
+
+## Example of matrix transformed into sign object
+aaa <- matrix(runif(100), 4)
+colnames(aaa) <- paste0('s', 1:25); rownames(aaa) <- paste0('sam', 1:4)
+to_sign(aaa)
+
+input_dummy <- matrix(runif(100), 4)
+colnames(input_dummy) <- paste0('s', 1:25); rownames(input_dummy) <- paste0('sam', 1:4)
+sign_dummy <- to_sign(input_dummy)
+add_together_matrix(sign_dummy)
+summarise(add_together_matrix(sign_dummy))
+
+summarise(to_sign(aaa))
+
+
+## example of comp_lm
+tmp_merged_compositional <- new("merged_compositional",
+                                id='adas',
+                                id_samples=c("sam1", "sam2", "sam3"),
+                                id_signatures= c('s1', 's2', 's3', 's4'), ## signature names
+                                count_matrix=MCMCpack::rdirichlet(3, c(1,1,1,1)),
+                                df=data.frame(a=c(3,4,1), b=c(10, 10, 10)))
+comp_lm(tmp_merged_compositional)
--- push_inputcommit.sh
+++ push_inputcommit.sh
@@ -4,7 +4,7 @@ git add .
 echo 'Everything added'
 echo 'Add text for commit:'
 read varname
-git commit -m $varname
+git commit -m "$varname"
 git push origin master
 echo Done
 
