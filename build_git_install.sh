e844020c32e62b32ab09b0e63903a4b47a9b0250
Lena M <lm687@cam.ac.uk>
2019-06-04 19:10:30 +0100

    del technical fi les

--- build_git_install.sh
+++
@@ -1,7 +0,0 @@
-## build the documentation with roxygen
-
-## upload it to github
-
-## download it using devtools
-
-
--- gitcommands.sh
+++
@@ -1,8 +0,0 @@
-git add README.md
-git commit -m 'readme'
-git push origin master
-
-git add vignette
-git commit -m 'documentation'
-git push origin master
-
