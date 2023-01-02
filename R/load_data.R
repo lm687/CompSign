85767d9056e4fcea65a812b0c10495b7e46e3880
Lena Morrill <lenamorrill@Lenas-MacBook-Pro.local>
2023-01-02 20:02:21 +0100

    add data

---
+++ R/load_data.R
@@ -0,0 +1,10 @@
+#' @format ## `PancEndocrine_signaturesMSE`
+#' A list of length three:
+#' \describe{
+#'   \item{Y}{Exposures of 140 samples (70 patients) and 13 signatures}
+#'   \item{x}{Match of samples and groups (second column), and including the intercept (first column)}
+#'   \item{z}{Match of patients and samples}
+#'   ...
+#' }
+#' @source https://dcc.icgc.org/pcawg
+"PancEndocrine_signaturesMSE"
