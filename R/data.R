9cdc397e9e6f6c2351435aceee4a384599eb8dbe
Morrill Gavarro <morrill1@lmed0873.mskcc.org>
2024-10-07 18:32:26 -0400

    data helper

--- R/data.R
+++ R/data.R
@@ -8,3 +8,15 @@
 #' }
 #' @source https://dcc.icgc.org/pcawg
 "PancEndocrine_signaturesMSE"
+
+
+#' @format ## `simplified_object`
+#' A list of length three:
+#' \describe{
+#'   \item{Y}{Exposures of 18 samples (9 patients) and 6 nucleotide changes}
+#'   \item{x}{Match of samples and groups (second column), and including the intercept (first column), as a binary matrix}
+#'   \item{z}{Match of patients and samples, as a binary matrix}
+#'   ...
+#' }
+#' @source https://dcc.icgc.org/pcawg
+"simplified_object"
