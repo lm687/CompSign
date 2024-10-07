efe71e49de9fe88ee4de679ea0131255bfbb5f16
Morrill Gavarro <morrill1@lmed0873.mskcc.org>
2024-10-07 19:04:31 -0400

    data helper

--- R/data.R
+++ R/data.R
@@ -13,7 +13,7 @@
 #' @format ## `simplified_object`
 #' A list of length three:
 #' \describe{
-#'   \item{Y}{Exposures of 18 samples (9 patients) and 6 nucleotide changes}
+#'   \item{Y}{Exposures of 34 samples (17 patients) and 6 nucleotide changes}
 #'   \item{x}{Match of samples and groups (second column), and including the intercept (first column), as a binary matrix}
 #'   \item{z}{Match of patients and samples, as a binary matrix}
 #'   ...
